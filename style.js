53fc1ef6cf21321fa5f41f1094d9ca1bd9ecabc0
Mónica del Corral <monica.delcorral@wildoom.com>
2019-01-11 10:06:19 +0100

    Initial commit

--- style.js
+++ style.js
@@ -1,1 +1,57 @@
+(function (blink) {
+  'use strict';
 
+  var PearsonheDevStyle = function () {
+    blink.theme.styles.basic.apply(this, arguments);
+  }
+
+  PearsonheDevStyle.prototype = {
+    parent: blink.theme.styles.aprendizaje_basado_en_proyectos.prototype,
+    bodyClassName: 'content_type_clase_pearsonhe_dev',
+    ckEditorStyles: {
+      name: 'pearsonhe-dev',
+      styles: []
+    },
+    init: function() {
+      this.parent.init.call(this.parent, this);
+    },
+    removeFinalSlide: function () {
+      this.parent.removeFinalSlide.call(this.parent, this, true);
+    },
+
+    /**
+    * Realiza operaciones al cargar los datos del curso.
+    * @param  {Object} data Información del curso.
+    */
+    onCourseDataLoaded: function(data) {
+      console.log("onCourseLoaded");
+    },
+
+    loadUserData: function() {
+      var urlSeguimiento = '/include/javascript/seguimientoCurso.js.php?idcurso=' + idcurso;
+      loadScript(urlSeguimiento, true, (function(data) {
+        window.actividades = actividades;
+      }).bind(this));
+    },
+
+  };
+
+
+  PearsonheDevStyle.prototype = _.extend({}, new blink.theme.styles.aprendizaje_basado_en_proyectos(), PearsonheDevStyle.prototype);
+
+  blink.theme.styles['pearsonhe-dev'] = PearsonheDevStyle;
+
+  blink.events.on('digitalbook:bpdfloaded', function () {
+    // Ejemplo carga de datos del curso desde un libro digital.
+    blink.getCourse(idcurso).done(function (data) {
+      var style = new PearsonheDevStyle;
+      style.onCourseDataLoaded(data);
+    });
+  });
+
+  // Remove Info
+  blink.events.on('indexLoaded', function(){
+    console.log("Index loaded");
+  });
+
+})( blink );
