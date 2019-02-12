f28eed555105c613ee9b203ad161c8ad2dcae8a1
Mónica del Corral <monica.delcorral@wildoom.com>
2019-02-12 16:34:23 +0100

    Sub-levels on menu and book index

--- style.js
+++ style.js
@@ -225,8 +225,10 @@ $(document).ready(function() {
 		} else {
 			var $sublevels = $(this).nextUntil('.pearsonhe-toc-unithead', 'li');
 			if ($(this).hasClass('pearsonhe-toc-active')) {
-				$sublevels.toggleClass('pearsonhe-toc-subunit-active');
+				$(this).removeClass('pearsonhe-toc-unithead-ancestor');
+				$sublevels.removeClass('pearsonhe-toc-subunit-active');
 			} else {
+				$(this).addClass('pearsonhe-toc-unithead-ancestor');
 				$sublevels.addClass('pearsonhe-toc-subunit-active');
 			}
 		}
