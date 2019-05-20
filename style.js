7be02fe0315ae4fa938752ceab85349b68dfce68
Mónica del Corral <monica.delcorral@wildoom.com>
2019-05-20 11:56:54 +0200

    FIX Book index

--- style.js
+++ style.js
@@ -180,11 +180,16 @@ pearsonheApp.customBookIndex = function() {
 
 	var $currentParent = $('#book-index .current-parent');
 	if (!$currentParent.hasClass('pearsonhe-toc-unithead')) {
-		$currentParent.addClass('pearsonhe-toc-subunit-active').prevUntil('.pearsonhe-toc-unithead', 'li').addClass('pearsonhe-toc-subunit-active');
+		$currentParent
+			.addClass('pearsonhe-toc-subunit-active')
+			.prevUntil('.pearsonhe-toc-unithead', 'li')
+				.addClass('pearsonhe-toc-subunit-active')
+				.end()
+			.nextUntil('.pearsonhe-toc-unithead', 'li')
+				.addClass('pearsonhe-toc-subunit-active');
 	} else {
 		$currentParent.addClass('pearsonhe-toc-active').nextUntil('.pearsonhe-toc-unithead', 'li').addClass('pearsonhe-toc-subunit-active');
 	}
-
 
 }
 
